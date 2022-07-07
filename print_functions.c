7bee122bee6339e616e8331a8f2310ad3def081a
viviani22 <vivianxhumetiku@gmail.com>
2022-07-07 00:32:22 -0700

    temp

--- print_functions.c
+++ print_functions.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
 int p_char(va_list arg)
 {
 	char c;
@@ -23,7 +24,7 @@ int p_string(va_list arg)
 	return (i);
 }
 
-int p_int(va_list arg)
+/** int p_int(va_list arg)
 {
 	return (1);
 }
@@ -32,4 +33,4 @@ int p_float(va_list arg)
 {
 	return (1);
 }
-
+*/
