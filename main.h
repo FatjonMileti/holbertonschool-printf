144cb0bee9e406fe673f5c5d08537815e252e0ec
FatjonMileti <tonimileti@yahoo.com>
2022-07-07 07:33:17 -0700

    temp

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * *
+ */
+typedef struct print {
+	char *op;
+	int(*p)(va_list arg);
+} print_t;
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+int p_char(va_list arg);
+int p_string(va_list arg);
+int p_int(va_list arg);
+int p_float(va_list arg);
+#endif
