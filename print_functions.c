ae4f0c64afcda52b80c6e9c8e14358c831975480
FatjonMileti <tonimileti@yahoo.com>
2022-07-07 07:29:32 -0700

    temp

---
+++ print_functions.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+/**
+ *
+ */
+int p_char(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, int);
+	_putchar(c);
+	return (1);
+}
+
+int p_string(va_list arg)
+{
+	char *s;
+	int i;
+
+	s = va_arg(arg, char*);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	return (i);
+}
+
+int p_int(va_list arg)
+{
+	int i, length;
+	
+	i = va_arg(arg, int);
+	for (length = 0; i != 0; length++)
+	{
+		_putchar(i % 10);
+		i = i / 10;
+	}
+	return (length);
+}
+/**
+int p_float(va_list arg)
+{
+	return (1);
+}
+*/
